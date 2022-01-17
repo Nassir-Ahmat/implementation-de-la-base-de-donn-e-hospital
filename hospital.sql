b2e7ecf57c7729db9b8eac275327083dcfcbca98
Nassir-Ahmat <nassirahmat601@gmail.com>
2022-01-17 14:45:18 +0100

    ajout de la base de données hospital

---
+++ hospital.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `docteur`
+--
+
+DROP TABLE IF EXISTS `docteur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `docteur` (
+  `id_D` int unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_D`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `docteur`
+--
+
+LOCK TABLES `docteur` WRITE;
+/*!40000 ALTER TABLE `docteur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `docteur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medicament`
+--
+
+DROP TABLE IF EXISTS `medicament`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medicament` (
+  `id_M` int unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_M`)
+) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicament`
+--
+
+LOCK TABLES `medicament` WRITE;
+/*!40000 ALTER TABLE `medicament` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medicament` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient` (
+  `id_P` int unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `ville` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_P`)
+) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prescription`
+--
+
+DROP TABLE IF EXISTS `prescription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prescription` (
+  `id_Pres` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_M` int unsigned NOT NULL,
+  `posologie` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_Pres`),
+  KEY `id` (`id_M`),
+  KEY `tbl_medicament` (`id_M`),
+  CONSTRAINT `tbl_medicament` FOREIGN KEY (`id_M`) REFERENCES `medicament` (`id_M`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prescription`
+--
+
+LOCK TABLES `prescription` WRITE;
+/*!40000 ALTER TABLE `prescription` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prescription` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `visite`
+--
+
+DROP TABLE IF EXISTS `visite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `visite` (
+  `id_V` int unsigned NOT NULL AUTO_INCREMENT,
+  `date_visite` varchar(45) NOT NULL,
+  `prix` varchar(30) NOT NULL,
+  `id_D` int unsigned NOT NULL,
+  `id_P` int unsigned NOT NULL,
+  PRIMARY KEY (`id_V`),
+  KEY `id` (`id_D`,`id_P`),
+  KEY `tbl_docteur` (`id_D`),
+  KEY `tbl_patient` (`id_P`),
+  CONSTRAINT `tbl_docteur` FOREIGN KEY (`id_D`) REFERENCES `docteur` (`id_D`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tbl_patient` FOREIGN KEY (`id_P`) REFERENCES `patient` (`id_P`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visite`
+--
+
+LOCK TABLES `visite` WRITE;
+/*!40000 ALTER TABLE `visite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `visite` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-17 14:34:19
